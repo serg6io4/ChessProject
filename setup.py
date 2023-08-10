64ae1a79e8b8c3ad9f0193ee0c18e12cffaf6c4c
serg6io4 <sergiocmn80@gmail.com>
2023-08-10 11:35:02 +0200

    Retoques6

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 import os
-import shutil
 import subprocess
 from git import Repo
 
 def main():
     repo_url = "https://github.com/serg6io4/ChessProject.git"
-    project_folder = "ChessProject"  # Cambia esto si deseas un nombre diferente para la carpeta del proyecto
+    project_folder = "/ruta/completa/a/tu/directorio/ChessProject"  # Cambia esto a la ruta deseada
 
     # Clona el repositorio de GitHub
-    Repo.clone_from(repo_url, project_folder)
+    if not os.path.exists(project_folder):
+        Repo.clone_from(repo_url, project_folder)
+    else:
+        print(f"El directorio '{project_folder}' ya existe.")
 
     # Cambia al directorio del proyecto
     os.chdir(project_folder)
 
     # Instala las dependencias
     subprocess.call(["pip", "install", "-r", "requirements.txt"])
-
-    # Crea la estructura de directorios si no existe
-    data_folder = "data"
-    if not os.path.exists(data_folder):
-        os.makedirs(data_folder)
 
     print("Proyecto descargado y configurado.")
 
